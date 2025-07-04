6ab37cfec6d65499fb1518a9a175ecab4a29d955
Tanchwa <dres2dogs@gmail.com>
2025-07-03 22:33:05 -0400

    moved create task function outside of main api file

---
+++ flaskr/api.py
@@ -0,0 +1,44 @@
+from flask import Flask, Response, request, jsonify, abort
+import hashlib
+import hmac
+import base64
+import os
+from datetime import datetime
+from reclaim_sdk.client import ReclaimClient
+from reclaim_sdk.resources.task import Task, TaskPriority, EventColor
+from reclaim_sdk.resources.hours import Hours
+from reclaim_sdk.exceptions import (
+    RecordNotFound,
+    InvalidRecord,
+    AuthenticationError,
+    ReclaimAPIError,
+)
+
+app = Flask(__name__)
+
+todoist_key = os.environ.get("TODOIST_KEY").encode()
+reclaim_key = os.environ.get("RECLAIM_KEY")
+
+ReclaimClient.configure(token=reclaim_key)
+
+@app.route('/webhook', methods=['POST'])
+def accept_webhook_request():
+    sha256sum_header = request.headers.get('X-Todoist-Hmac-SHA256')
+
+    request_body = request.data
+    digest = hmac.new(todoist_key, request_body, digestmod=hashlib.sha256).digest()
+    signature = base64.b64encode(digest).decode()
+
+    if not hmac.compare_digest(sha256sum_header, signature):
+        abort(401, description="Unable to verify integrity of incomming request")
+
+    else:
+        todoist_task = request.json
+        print(todoist_task)
+
+        event_data = todoist_task["event_data"]
+
+        todoist_due_date = event_data["due"]["date"]
+        arg1,arg2,arg3 = map(int, todoist_due_date.split("-"))
+        reclaim_due_date = datetime(arg1,arg2,arg3)
+        return Response(status=200)
